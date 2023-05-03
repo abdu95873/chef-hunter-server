6fbfdffff3aebd58fb0d0cfe89a3fd744669dc86
abdu95873 <abdu95873@gmail.com>
2023-05-03 11:25:27 +0600

    update data

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const cors = require('cors');
 
 
 app.use(cors());
+const chef = require('./Data/chef/chef.json');
 
 app.get ('/', (req, res) =>{
     res.send('i am running')
 });
 
+
+app.get ('/chef', (req, res) =>{
+    res.send(chef);
+});
+
+app.get ('/chef/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedChef =chef.find(n=>n._id === id);
+    res.send(selectedChef); 
+    console.log(selectedChef);
+});
 
 app.listen(port, ()=>{
     console.log(`i am running on port :${port}`)
